4ec0650426c3964f22ea1d10e934a1f8f230efb9
kasia <kasiaczarna051@gmail.com>
2022-07-11 20:25:42 +0200

    init repo

---
+++ scripts/points_in_line.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+from check_line.srv import PointsInLine, PointsInLineResponse
+import rospy
+
+def points_in_line_server():
+    
+
+
+if __name__ == "__main__":
+    points_in_line_server()
